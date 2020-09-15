1cc55d56d8149c5d5ba00fe3f23f4625744f8f2b
MrDock21 <dan.mejia@alumnos.udg.mx>
2020-09-15 16:01:10 -0500

    Implementada la conversión de Fahrenheit a Celcius

--- Fahrenheit a Celcius/fahrenheit_celcius.go
+++ Fahrenheit a Celcius/fahrenheit_celcius.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var side int = 1
-	side *= 2
-	fmt.Println("¡Hola, Mundo!")
+	var input string
+	var fahrenheit int = 1
+
+	fmt.Print(`
+	+---------FAHRENHEIT A CELCIUS----------+
+	  Ingrese los grados fahrenheit: `)
+
+	// Read from console
+	_, err := fmt.Scan(&input)
+
+	if err == nil {
+		// Scan values from input string
+		integer, atoi_err := strconv.Atoi(input)
+		if atoi_err == nil {
+			fahrenheit = integer
+			fmt.Printf("\t\tGrados Celcius = 5/9 x (%d - 32) = %.3f\n", fahrenheit, (5.0/9.0)*float32(fahrenheit-32))
+		} else {
+			fmt.Println("\t\tValor inválido!")
+		}
+	} else {
+		fmt.Println(err)
+	}
 }
