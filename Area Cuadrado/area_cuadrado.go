1ce1c7b8a023c3385d74ddaee4f2b9a83c88355f
MrDock21 <dan.mejia@alumnos.udg.mx>
2020-09-15 15:09:09 -0500

    Implementado el cálculo del área del cuadrado

--- Area Cuadrado/area_cuadrado.go
+++ Area Cuadrado/area_cuadrado.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
+	var input string
 	var side int = 1
-	side *= 2
-	fmt.Println("¡Hola, Mundo!")
+
+	fmt.Print(`
+	+---------AREA DEL CUADRADO----------+
+	  Ingrese el lado del cuadrado: `)
+
+	// Read from console
+	_, err := fmt.Scan(&input)
+
+	if err == nil {
+		// Scan values from input string
+		integer, atoi_err := strconv.Atoi(input)
+		if atoi_err == nil {
+			side = integer
+			fmt.Printf("\t\tEl Área = %d x %d = %d\n", side, side, side*side)
+		} else {
+			fmt.Println("\t\tValor inválido!")
+		}
+	} else {
+		fmt.Println(err)
+	}
 }
