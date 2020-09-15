fbf1ced53bf5e86d562bc00fe466ee838d3f219e
MrDock21 <dan.mejia@alumnos.udg.mx>
2020-09-15 15:20:28 -0500

    Implementado el cálculo del área del circulo

--- Area Circulo/area_circulo.go
+++ Area Circulo/area_circulo.go
@@ -1,9 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var side int = 1
-	side *= 2
-	fmt.Println("¡Hola, Mundo!")
+	var input string
+	var radius int = 1
+	const PI = 3.141592
+
+	fmt.Print(`
+	+---------AREA DEL CÍRCULO----------+
+	  Ingrese la radio del círculo: `)
+
+	// Read from console
+	_, err := fmt.Scan(&input)
+
+	if err == nil {
+		// Scan values from input string
+		integer, atoi_err := strconv.Atoi(input)
+		if atoi_err == nil {
+			radius = integer
+			fmt.Printf("\t\tÁrea = PI x %d x %d = %.3f\n", radius, radius, PI*float32(radius)*float32(radius))
+		} else {
+			fmt.Println("\t\tValor inválido!")
+		}
+	} else {
+		fmt.Println(err)
+	}
 }
