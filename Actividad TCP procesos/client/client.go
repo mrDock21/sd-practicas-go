262cb8fca6d5b53245ffed40d90ac784d2254b1b
MrDock21 <dan.mejia@alumnos.udg.mx>
2020-11-04 20:28:09 -0600

    Documentado el código

--- Actividad TCP procesos/client/client.go
+++ Actividad TCP procesos/client/client.go
@@ -11,12 +11,14 @@ import (
 	connData "../ConnData"
 )
 
+//	Clears console
 func clear() {
 	clear := exec.Command("clear")
 	clear.Stdout = os.Stdout
 	clear.Run()
 }
 
+//	General printer for error messages
 func isThereError(err error) bool {
 	if err != nil {
 		fmt.Println(err)
@@ -25,15 +27,18 @@ func isThereError(err error) bool {
 	return false
 }
 
+//	Process individual process given by server. Stops after receiving stopping signal (channel)
 func process(connectionDone, stopFlag chan bool, p connData.Process) {
 	isStopped := false
 
 	for !isStopped {
+		// Listen for channels
 		select {
 		case stop := <-stopFlag:
 			isStopped = stop
 		default: //Keep going
 		}
+		// Process updating
 		p.Update()
 		fmt.Println(p.ToString() + "\n")
 		time.Sleep(time.Millisecond * 500)
@@ -41,12 +46,15 @@ func process(connectionDone, stopFlag chan bool, p connData.Process) {
 	}
 	// Send done signal to server
 	conn, _ := net.Dial("tcp", ":9876")
+	// Give back process to server
 	isThereError(gob.NewEncoder(conn).Encode(connData.ConnData{MsgType: 1, Task: p}))
 	conn.Close()
 	fmt.Println("[CLIENT]	Connection closed!\n")
+	// Allow main to finish
 	connectionDone <- true
 }
 
+//	Connects to server and receives process from it.
 func connect(connectionDone, stopFlag chan bool) {
 	c, err := net.Dial("tcp", ":9876")
 
@@ -60,18 +68,19 @@ func connect(connectionDone, stopFlag chan bool) {
 			Id: 0, Time: 0,
 		},
 	}
-	// send
+	// send initial message to server
 	fmt.Println("[CLIENT]	Connecting...")
 	if isThereError(gob.NewEncoder(c).Encode(data)) {
 		c.Close()
 		fmt.Println(err)
 	}
-	// receive
+	// receive process from server
 	if isThereError(gob.NewDecoder(c).Decode(&data)) {
 		c.Close()
 		return
 	}
 	c.Close()
+	// start computing given process
 	go process(connectionDone, stopFlag, data.Task)
 }
 
@@ -83,7 +92,7 @@ func main() {
 	connectionDoneFlag := make(chan bool)
 
 	go connect(connectionDoneFlag, stopFlag)
-
+	// wait until user presses "enter"
 	fmt.Scanln(&input)
 	// Send stopping signal
 	stopFlag <- true
--- Actividad TCP procesos/server.go
+++ Actividad TCP procesos/server.go
@@ -11,12 +11,14 @@ import (
 	connData "./ConnData"
 )
 
+//	Clears console window
 func clear() {
 	clear := exec.Command("clear")
 	clear.Stdout = os.Stdout
 	clear.Run()
 }
 
+// Updates all processes' process time
 func updateTimers(processes *[]connData.Process) {
 	for i := 0; i < len(*processes); i++ {
 		(*processes)[i].Update()
@@ -25,10 +27,11 @@ func updateTimers(processes *[]connData.Process) {
 	fmt.Println("")
 }
 
+//	Main process loop goroutine. Stops until stopFlag receives "true" signal
 func process(stopFlag chan bool, processes *[]connData.Process) {
 	isProcessInterrupted := false
 	for {
-
+		// Listen channels
 		select {
 		case stop := <-stopFlag:
 			isProcessInterrupted = stop
@@ -36,6 +39,7 @@ func process(stopFlag chan bool, processes *[]connData.Process) {
 		}
 
 		if !isProcessInterrupted {
+			// Only update processes times while in safe mode
 			updateTimers(processes)
 			time.Sleep(time.Millisecond * 500)
 			clear()
@@ -43,7 +47,10 @@ func process(stopFlag chan bool, processes *[]connData.Process) {
 	}
 }
 
+//	Sends process to client at index 0 of processes'
+//	slice and removes it from main slice
 func sendProcessToClient(connection net.Conn, processes *[]connData.Process) {
+	// Make copy of first process
 	sentProcess := connData.Process{
 		Id: (*processes)[0].Id, Time: (*processes)[0].Time,
 	}
@@ -55,13 +62,15 @@ func sendProcessToClient(connection net.Conn, processes *[]connData.Process) {
 	for i := 0; i < len(*processes)-1; i++ {
 		(*processes)[i] = (*processes)[i+1]
 	}
-	// truncate
+	// truncate slice (removing last element)
 	*processes = (*processes)[:len(*processes)-1]
 	// send process to client
 	gob.NewEncoder(connection).Encode(response)
 }
 
+//	Main server loop. Listens for client's messages
 func serve() {
+	// Initial hardcoded processes
 	processes := []connData.Process{
 		connData.Process{Id: 1, Time: 0},
 		connData.Process{Id: 2, Time: 0},
@@ -71,7 +80,7 @@ func serve() {
 	}
 
 	stopFlag := make(chan bool)
-
+	// Start server
 	server, err := net.Listen("tcp", ":9876")
 
 	if err != nil {
@@ -80,10 +89,11 @@ func serve() {
 	}
 
 	fmt.Println("[SERVER]	Listening...")
-
+	// Start counting processes' times
 	go process(stopFlag, &processes)
 
 	for {
+		// wait for client's connection
 		connection, err := server.Accept()
 
 		if err != nil {
@@ -95,8 +105,9 @@ func serve() {
 	}
 }
 
+//	Decides which operation to make according to client's message
 func handleClient(connection net.Conn, interruptProcess chan bool, processes *[]connData.Process) {
-	// GUARD
+	// GUARD (avoids deleting processes while been processed)
 	interruptProcess <- true
 
 	var data connData.ConnData
@@ -104,12 +115,12 @@ func handleClient(connection net.Conn, interruptProcess chan bool, processes *[]
 	gob.NewDecoder(connection).Decode(&data)
 
 	if data.MsgType > 0 {
-		fmt.Println("[SERVER]	RECEIVES PROCESS")
 		// Receiving process
+		fmt.Println("[SERVER]	RECEIVES PROCESS")
 		*processes = append(*processes, data.Task)
 	} else {
-		fmt.Println("[SERVER]	SENDS PROCESS")
 		// Send process
+		fmt.Println("[SERVER]	SENDS PROCESS")
 		sendProcessToClient(connection, processes)
 	}
 
